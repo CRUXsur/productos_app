b00ed34fcea4ef093560c7d51e0632f4ceec068b
CRUXsur <marcojtorrico@hotmail.com>
2022-03-09 10:28:44 -0400

    Widget-ProductCard

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -8,10 +8,68 @@ class ProductCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Container(
-        margin: EdgeInsets.only(top: 30, bottom: 50),
+        margin: const EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        decoration: _cardBorders(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: const [
+            _BackgroundImage(),
+            _ProductDetails(),
+          ],
+        ),
+
+        //color: Colors.red,
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 7),
+            blurRadius: 10,
+          ),
+        ],
+      );
+}
+
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      color: Colors.red,
+    );
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: const FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
