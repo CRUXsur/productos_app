e06b410a8442524a5a716fe70c4f1d2d00f5ca61
CRUXsur <marcojtorrico@hotmail.com>
2022-03-11 11:19:47 -0400

    Manejo de imagenes nulas

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -192,12 +192,18 @@ class _BackgroundImage extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
-          // TODO: Fix productos cuando no hay imagen
-          placeholder: const AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage(url!),
-          fit: BoxFit.cover,
-        ),
+        child: url == null
+            ? const Image(
+                image: AssetImage('assets/no-image.png'),
+                fit: BoxFit.cover,
+              )
+            : FadeInImage(
+                //ternario
+                // Done: Fix productos cuando no hay imagen
+                placeholder: const AssetImage('assets/jar-loading.gif'),
+                image: NetworkImage(url!),
+                fit: BoxFit.cover,
+              ),
       ),
     );
   }
